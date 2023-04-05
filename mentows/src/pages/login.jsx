939e1cff32757cf735387aa5ed1d1b332d846309
Keith Austin Jr <keiaus@Keiths-MacBook-Pro.local>
2023-04-05 12:52:48 -0400

    Finished login page

--- mentows/src/pages/login.jsx
+++ mentows/src/pages/login.jsx
@@ -5,18 +5,18 @@ const Login = () => {
     return (
         <div>
             <title>
-                Log In
+                Login
             </title>
-                <div id='style-container' style={{height:'500px', width:'500px', backgroundColor:'#007FFF', margin:'auto', borderRadius:'100%'}}>
-                    <div id='container' style={{textAlign:'center'}}>
-                        <h1 style={{textAlign:'center', marginTop:'100px'}}>Login</h1>
-                        <input type="text" name="username" placeholder='Username' id="username" style={{marginTop:'100px', height:'40px', width:'340px', fontSize:'30px'}}/>
+                <div id='style-container' style={{height:'500px', width:'500px', backgroundColor:'#007FFF', margin:'auto', borderRadius:'10%'}}>
+                    <div id='container' style={{textAlign:'center', marginTop:'100px'}}>
+                        <h1 style={{textAlign:'center', paddingTop:'50px'}}>Login</h1>
+                        <input type="text" name="username" placeholder='Username' id="username" style={{height:'40px', width:'340px', fontSize:'30px', marginTop:'50px'}}/>
                         <br />
                         <br />
-                        <input type="password" name="password" placeholder='Password'  id="password" style={{height:'40px', width:'340px', fontSize:'30px'}}/>
+                        <input type="password" name="password" placeholder='Password' id="password" style={{height:'40px', width:'340px', fontSize:'30px'}}/>
                         <br />
                         <br />
-                        <button id='submit' style={{height:'40px', width:'150px', fontSize:'30px'}}>Submit</button>
+                        <button id='submit' style={{height:'40px', width:'150px', fontSize:'30px', marginTop:'100px'}}>Login</button>
                     </div>
                 </div>
         </div>
--- mentows/src/pages/signup.jsx
+++ mentows/src/pages/signup.jsx
@@ -5,8 +5,20 @@ const SignUp = () => {
     return (
         <head>
             <title>
-                Sign Up
+                Signup
             </title>
+            <div id='style-container' style={{height:'500px', width:'500px', backgroundColor:'#007FFF', margin:'auto', borderRadius:'10%'}}>
+                    <div id='container' style={{textAlign:'center', marginTop:'100px'}}>
+                        <h1 style={{textAlign:'center', paddingTop:'50px'}}>Signup</h1>
+                        <input type="text" name="username" placeholder='Username' id="username" style={{height:'40px', width:'340px', fontSize:'30px', marginTop:'50px'}}/>
+                        <br />
+                        <br />
+                        <input type="password" name="password" placeholder='Password' id="password" style={{height:'40px', width:'340px', fontSize:'30px'}}/>
+                        <br />
+                        <br />
+                        <button id='submit' style={{height:'40px', width:'150px', fontSize:'30px', marginTop:'100px'}}>Signup</button>
+                    </div>
+                </div>
         </head>
     );
 };
