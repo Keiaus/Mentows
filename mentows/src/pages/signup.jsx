3d53736a967a5dea65f2f40ea5b29215206189f7
keiaus <k.aus2029@gmail.com>
2023-04-07 09:23:22 -0400

    Signup page fixes

--- mentows/src/pages/signup.jsx
+++ mentows/src/pages/signup.jsx
@@ -26,74 +26,84 @@ const SignUp = () => {
                         <input type="text" name="phone" placeholder='Phone' id="phone" style={{height:'40px', width:'340px', fontSize:'30px'}}/>
                         <br />
                         <br />
-                        <label htmlFor="DOB" style={{fontSize:'30px'}}>Date of Birth</label>
+                            <div style={{marginTop:'50px'}}>
+                                <label htmlFor="DOB" style={{fontSize:'30px'}}>Date of Birth</label>
+                            </div>
                         <br />
-                        <div class="month" style={{paddingRight:'10px', marginRight:'10px', marginTop:'30px'}}>
-                            <select id='month-select' placeholder='Month' style={{width:'100px'}}>
-                                <label style={{fontSize:'200px'}}>Month</label>
-                                <option value=''></option>
-                                <option value='January'>January</option>
-                                <option value='February'>February</option>
-                                <option value='March'>March</option>
-                                <option value='April'>April</option>
-                                <option value='May'>May</option>
-                                <option value='June'>June</option>
-                                <option value='July'>July</option>
-                                <option value='August'>August</option>
-                                <option value='September'>September</option>
-                                <option value='October'>October</option>
-                                <option value='November'>November</option>
-                                <option value='December'>December</option>
-                            </select>
-                        </div>
+                        <div id='selecion' style={{display:'inline-flex', textAlign:'center', marginTop:'10px'}}>
+                            <div class="month" style={{paddingRight:'10px'}}>
+                                <label style={{fontSize:'15px'}}>Month</label>
+                                <br />
+                                <select id='month-select' style={{width:'100px'}}>
+                                    <option value=''></option>
+                                    <option value='January'>January</option>
+                                    <option value='February'>February</option>
+                                    <option value='March'>March</option>
+                                    <option value='April'>April</option>
+                                    <option value='May'>May</option>
+                                    <option value='June'>June</option>
+                                    <option value='July'>July</option>
+                                    <option value='August'>August</option>
+                                    <option value='September'>September</option>
+                                    <option value='October'>October</option>
+                                    <option value='November'>November</option>
+                                    <option value='December'>December</option>
+                                </select>
+                            </div>
 
-                        {/* <div class="dropdown2">
-                            <button class="daybtn">Day</button>
-                            <div class="dropdown-content2">
-                                <a href="#">1</a>
-                                <a href="#">2</a>
-                                <a href="#">3</a>
-                                <a href="#">4</a>
-                                <a href="#">5</a>
-                                <a href="#">6</a>
-                                <a href="#">7</a>
-                                <a href="#">8</a>
-                                <a href="#">9</a>
-                                <a href="#">10</a>
-                                <a href="#">11</a>
-                                <a href="#">12</a>
-                                <a href="#">13</a>
-                                <a href="#">14</a>
-                                <a href="#">15</a>
-                                <a href="#">16</a>
-                                <a href="#">17</a>
-                                <a href="#">18</a>
-                                <a href="#">19</a>
-                                <a href="#">20</a>
-                                <a href="#">21</a>
-                                <a href="#">22</a>
-                                <a href="#">23</a>
-                                <a href="#">24</a>
-                                <a href="#">25</a>
-                                <a href="#">26</a>
-                                <a href="#">27</a>
-                                <a href="#">28</a>
-                                <a href="#">29</a>
-                                <a href="#">30</a>
-                                <a href="#">31</a>
+                            <div class="day" style={{paddingRight:'10px'}}>
+                                <label style={{fontSize:'15px'}}>Day</label>
+                                <br />
+                                <select id='day-select' style={{width:'100px'}}>
+                                    <option value=''></option>
+                                    <option value='one'>1</option>
+                                    <option value='two'>2</option>
+                                    <option value='three'>3</option>
+                                    <option value='four'>4</option>
+                                    <option value='five'>5</option>
+                                    <option value='six'>6</option>
+                                    <option value='seven'>7</option>
+                                    <option value='eight'>8</option>
+                                    <option value='nine'>9</option>
+                                    <option value='ten'>10</option>
+                                    <option value='eleven'>11</option>
+                                    <option value='twelve'>12</option>
+                                    <option value='thirteen'>13</option>
+                                    <option value='fourteen'>14</option>
+                                    <option value='fifteen'>15</option>
+                                    <option value='sixteen'>16</option>
+                                    <option value='seventeen'>17</option>
+                                    <option value='eighteen'>18</option>
+                                    <option value='nineteen'>19</option>
+                                    <option value='twenty'>20</option>
+                                    <option value='twentyone'>21</option>
+                                    <option value='twentytwo'>22</option>
+                                    <option value='twentythree'>23</option>
+                                    <option value='twentyfour'>24</option>
+                                    <option value='twentyfive'>25</option>
+                                    <option value='twentysix'>26</option>
+                                    <option value='twentyseven'>27</option>
+                                    <option value='twentyeight'>28</option>
+                                    <option value='twentynine'>29</option>
+                                    <option value='thirty'>30</option>
+                                    <option value='thirtyone'>31</option>
+                                </select>
                             </div>
+
+                            <div class="year" style={{paddingRight:'10px'}}>
+                                <label style={{fontSize:'15px'}}>Year</label>
+                                <br />
+                                <select id='year-select' style={{width:'100px'}}>
+                                    <option value=''></option>
+                                    <option value=''>{yearLinks}</option>
+                                </select>
+                            </div>
+
                         </div>
                         <br />
-                        <br />
-                        <div class="dropdown3">
-                            <button class="yearbtn">Year</button>
-                            <div class="dropdown-content3">
-                                {yearLinks.join('\n')}
+                            <div style={{marginTop:'50px'}}>
+                                <button id='next' style={{height:'40px', width:'150px', fontSize:'30px', marginTop:'50px', borderRadius:'3%'}}>Next</button>
                             </div>
-                        </div>
-                        <br />
-                        <br /> */}
-                        <button id='next' style={{height:'40px', width:'150px', fontSize:'30px', marginTop:'50px', borderRadius:'3%'}}>Next</button>
                     </div>
                 </div>
             </div>
