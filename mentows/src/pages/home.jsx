dc6022f6fca5e06e51638faf07f443107e3fc537
Keith Austin Jr <keiaus@Keiths-MacBook-Pro.local>
2023-05-01 16:38:58 -0400

    Still trying to fix search bar

--- mentows/src/pages/home.jsx
+++ mentows/src/pages/home.jsx
@@ -56,8 +56,8 @@ const Home = () => {
                     <div id="searchbar" style={{ textAlign: 'center', marginTop: '50px', alignContent:"center"}}>
                         {/* This form navigates to a page based on search entry */}
                         <form action="" method="get">
-                            <input type="text" style={{height: '40px', width: '400px', fontSize: '20px'}} />
-                            <button style={{ fontSize: '15px', margin:"auto"}}><i className="fa-solid fa-atom fa-spin" style={{fontSize:"30px", color: "coral"}}></i></button>
+                            <input type="text" style={{height: '40px', width: '431px', fontSize: '20px', outline:"none"}} />
+                            <button style={{ fontSize: '15px', margin:"auto"}}><i className="fa-solid fa-atom fa-spin" style={{fontSize:"43px", color: "coral"}}></i></button>
                         </form>
                     </div>
 
